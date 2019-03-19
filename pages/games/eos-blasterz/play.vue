fff9a3a300da0464d9e79516ef16a49716b5feef
yjh <yunjh1994@gmail.com>
2019-03-19 19:48:24 +0900

    다운로드 경로 수정 (v12 -> 릴리즈)

--- pages/games/eos-blasterz/play.vue
+++ pages/games/eos-blasterz/play.vue
@@ -9,7 +9,7 @@
         <div class="btn_area">
             <!-- <a href="#" class="play ios">iOS Play</a> -->
             <a href="https://play.google.com/store/apps/details?id=com.JellyJuice.Blasterz" class="play aos">Google Play</a>
-            <a href="http://syncmedia.macc.skcdn.com/_assembly/Blasterz-release_v12.apk" class="download apk">Android APK</a>
+            <a href="http://syncmedia.macc.skcdn.com/_assembly/Blasterz-release.apk" class="download apk">Android APK</a>
         </div>
     </div>
 </section>
